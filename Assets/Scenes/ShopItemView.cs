ec0f060f78a77acb15d6757a3a002d1d310fb328
SelimJB <selim.bennani@gmail.com>
2025-06-27 22:18:19 +0100

    Create ShopItem prefab and shop menu UI

---
+++ Assets/Scenes/ShopItemView.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyGame.Shop
+{
+	public class ShopItemView : MonoBehaviour
+	{
+		[SerializeField] private TextMeshProUGUI itemNameText;
+		[SerializeField] private TextMeshProUGUI itemPriceText;
+		[SerializeField] private TextMeshProUGUI itemDescriptionText;
+		[SerializeField] private Image itemIcon;
+
+		private ShopItem item;
+
+		public void Initialize(ShopItem shopItem)
+		{
+			item = shopItem;
+			itemNameText.text = item.Item.Name;
+			itemPriceText.text = item.Price.ToString("F0", CultureInfo.InvariantCulture) + " $";
+			itemDescriptionText.text = item.Item.Description;
+			itemIcon.sprite = item.Item.Icon;
+		}
+	}
+}
